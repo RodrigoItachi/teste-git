12b8bfb580d00ec2acec1d2488693a5ea7f58f3d
RodrigoItachi <sousarodrigo56@outlook.com>
2020-12-31 07:19:22 -0300

    Criando projeto

---
+++ src/course/Program.java
@@ -0,0 +1,11 @@
+package course;
+
+public class Program {
+
+	public static void main(String[] args) {
+		System.out.println("Teste");
+		System.out.println("Teste");
+		System.out.println("Teste03");
+	}
+
+}
