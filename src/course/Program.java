70a6b599150380d1dfb0c5439837c851e2c6af62
RodrigoItachi <sousarodrigo56@outlook.com>
2020-12-31 07:24:20 -0300

    alteração nas saídas de dados

--- src/course/Program.java
+++ src/course/Program.java
@@ -3,8 +3,8 @@ package course;
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Teste");
-		System.out.println("Teste");
+		System.out.println("Teste01");
+		System.out.println("Teste02");
 		System.out.println("Teste03");
 	}
 
